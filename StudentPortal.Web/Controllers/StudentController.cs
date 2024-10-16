6e6264bb8119959ce6ab998027fb6116a93b66ae
Sarika <sarika.kamble@kemarautomation.in>
2024-10-16 12:59:35 +0530

    InitialCommit

---
+++ StudentPortal.Web/Controllers/StudentController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web.Data;
+using StudentPortal.Web.Models;
+using StudentPortal.Web.Models.Entities;
+
+namespace StudentPortal.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public StudentController(ApplicationDBContext dbcontext)
+        {
+            _dbContext = dbcontext;  
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel model)
+        {
+            var duplicate = _dbContext.Students.Where(x=>x.Phone ==model.Phone).FirstOrDefaultAsync();
+            if(duplicate != null)
+            {
+                ViewBag.Message = "A student with this phone number already exists.";
+                return View(new AddStudentViewModel());
+            }
+            var student = new Student
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Phone = model.Phone,
+                Subscribed = model.Subscribed,
+            };
+            await _dbContext.Students.AddAsync(student);
+            await _dbContext.SaveChangesAsync();
+            ViewBag.Message = "Student added successfully.";
+            return View(new AddStudentViewModel());
+        }
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+          var stdlist= await _dbContext.Students.ToListAsync();
+            return View(stdlist);
+        }
+    }
+}
---
+++ StudentPortal.Web/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using StudentPortal.Web.Models.Entities;
+
+namespace StudentPortal.Web.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options) 
+        {
+                
+        }
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
